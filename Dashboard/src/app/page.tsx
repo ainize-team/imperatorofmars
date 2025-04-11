e89886a6928e59f2edec7ed2aa3bdf59295162f0
jiyoung <jiyoungan@comcom.ai>
2025-04-12 01:55:14 +0900

    feat: add web3 wallet connect

--- Dashboard/src/app/page.tsx
+++ Dashboard/src/app/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+import Navbar from "@/components/sections/Navbar";
 import FeedVeiwer from "@/components/feedViewer";
 import FOLViewer from "@/components/folViewer";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-full w-full gap-5">
-      <h1 className="font-bold text-4xl">Imperator of Mars</h1>
+      <Navbar />
       {/* Contents */}
       <div className="flex flex-row gap-4">
         <FOLViewer />
