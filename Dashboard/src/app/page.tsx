0012f34b45066f726137dbeaba97129151709ec7
Yoojin Ko <db232100@gmail.com>
2025-04-11 19:08:35 +0900

    feat: foi and feed viewer with mok

--- Dashboard/src/app/page.tsx
+++ Dashboard/src/app/page.tsx
@@ -1,12 +1,15 @@
+import FeedVeiwer from "@/components/feedViewer";
+import FOIViewer from "@/components/foiViewer";
+
 export default function Home() {
   return (
     <div className="flex flex-col h-full w-full gap-5">
       <h1 className="font-bold text-4xl">Imperator of Mars</h1>
       {/* Contents */}
       <div className="flex flex-row gap-4">
-        <div className="flex-1 border-2 border-black h-[80vh]">FOI</div>
+        <FOIViewer />
         <div className="flex-1 border-2 border-black h-[80vh]">Dag visualizer</div>
-        <div className="flex-1 border-2 border-black h-[80vh]">Feed</div>
+        <FeedVeiwer />
       </div>
       {/* Input */}
       <div className="flex flex-row border-2 border-black w-full justify-between p-1">
---
+++ Dashboard/src/components/foiViewer.tsx
@@ -0,0 +1,17 @@
+import mokFOI from "@/moks/mok_foi"
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+
+export default function FOIViewer() {
+  
+  return (
+    <div className="flex-1 border-2 border-black h-[80vh] overflow-hidden p-2">
+      <div>FOI</div>
+      <div className="flex flex-col w-full h-full overflow-scroll mx-2 text-[0.75rem]">
+        <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+          {"```" + mokFOI + "```"}
+        </ReactMarkdown>
+      </div>
+    </div>
+  )
+}
