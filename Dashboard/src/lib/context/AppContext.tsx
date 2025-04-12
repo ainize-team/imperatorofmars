95339488ca8227e2d36a7dafd9c6ce7ed688eb79
jiyoung <jiyoungan@comcom.ai>
2025-04-12 15:02:51 +0900

    fix: remove unused code

--- Dashboard/src/lib/context/AppContext.tsx
+++ Dashboard/src/lib/context/AppContext.tsx
@@ -1,22 +1,14 @@
 "use client";
 import { PropsWithChildren, createContext, useEffect, useContext, useState } from "react";
-import { Address, createPublicClient, createWalletClient, custom } from "viem";
+import { custom } from "viem";
 import { useWalletClient } from "wagmi";
-import { defaultNftContractAbi } from "../defaultNftContractAbi";
-import { aeneid, StoryClient, StoryConfig } from "@story-protocol/core-sdk";
-import { NFT_CONTRACT_ADDRESS } from "../constants";
+import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
 
 interface AppContextType {
   txLoading: boolean;
   txHash: string;
-  txName: string;
-  transactions: { txHash: string; action: string; data: any }[];
   client: StoryClient | undefined;
-  setTxLoading: (loading: boolean) => void;
   setTxHash: (txHash: string) => void;
-  setTxName: (txName: string) => void;
-  mintNFT: (to: Address, uri: string) => Promise<number | null>;
-  addTransaction: (txHash: string, action: string, data: any) => void;
 }
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
@@ -30,12 +22,6 @@ export const useStory = () => {
 };
 
 export default function AppProvider({ children }: PropsWithChildren) {
-  const [txLoading, setTxLoading] = useState<boolean>(false);
-  const [txName, setTxName] = useState<string>("");
-  const [txHash, setTxHash] = useState<string>("");
-  const [transactions, setTransactions] = useState<{ txHash: string; action: string; data: any }[]>(
-    []
-  );
   const { data: wallet } = useWalletClient();
   const [client, setClient] = useState<StoryClient | undefined>(undefined);
 
@@ -49,46 +35,6 @@ export default function AppProvider({ children }: PropsWithChildren) {
     return client;
   };
 
-  const mintNFT = async (to: Address, uri: string): Promise<number | null> => {
-    if (!wallet?.account.address) return null;
-    console.log("Minting a new NFT...");
-    console.log(to, uri);
-
-    const walletClient = createWalletClient({
-      account: wallet.account,
-      chain: aeneid,
-      transport: custom(wallet!.transport),
-    });
-    const publicClient = createPublicClient({
-      transport: custom(wallet!.transport),
-      chain: aeneid,
-    });
-
-    const { request } = await publicClient.simulateContract({
-      address: NFT_CONTRACT_ADDRESS,
-      functionName: "mintNFT",
-      args: [to, uri],
-      abi: defaultNftContractAbi,
-    });
-    const hash = await walletClient.writeContract(request);
-    console.log(`Minted NFT successful with hash: ${hash}`);
-
-    const { logs } = await publicClient.waitForTransactionReceipt({
-      hash,
-    });
-    if (logs[0].topics[3]) {
-      let tokenId = parseInt(logs[0].topics[3], 16);
-      console.log(`Minted NFT tokenId: ${tokenId}`);
-      addTransaction(hash, "Mint NFT", { tokenId });
-      return tokenId;
-    }
-    return null;
-  };
-
-  const addTransaction = (txHash: string, action: string, data: any) => {
-    setTransactions((oldTxs) => [...oldTxs, { txHash, action, data }]);
-  };
-
   useEffect(() => {
     if (!client && wallet?.account.address) {
       let newClient = setupStoryClient();
@@ -96,22 +42,5 @@ export default function AppProvider({ children }: PropsWithChildren) {
     }
   }, [wallet]);
 
-  return (
-    <AppContext.Provider
-      value={{
-        txLoading,
-        txHash,
-        txName,
-        transactions,
-        setTxLoading,
-        setTxName,
-        setTxHash,
-        mintNFT,
-        addTransaction,
-        client,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
+  return <AppContext.Provider value={{ client }}>{children}</AppContext.Provider>;
 }
