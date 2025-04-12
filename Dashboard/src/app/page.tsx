6f7cd611a3df17576c1dac74bb4587fcdcd3b18d
jiyoung <jiyoungan@comcom.ai>
2025-04-12 12:15:37 +0900

    feat: add wallet signing

--- Dashboard/src/app/page.tsx
+++ Dashboard/src/app/page.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
-import DagVisualizer from "@/components/dagVisualizer";
-import toast from "react-hot-toast";
 import { useState } from "react";
+import { useSignMessage } from "wagmi";
+import toast from "react-hot-toast";
 import Navbar from "@/components/sections/Navbar";
 import FeedVeiwer from "@/components/feedViewer";
 import FOLViewer from "@/components/folViewer";
+import DagVisualizer from "@/components/dagVisualizer";
 import { generateCid } from "@/utils/crypto";
 
 export default function Home() {
@@ -14,14 +15,15 @@ export default function Home() {
   const [nodes, setNodes] = useState<any>([]);
   const [links, setLinks] = useState<any>([]);
   const [selectedNodes, setSelectedNodes] = useState<any>([]); // Change to array
+  const { signMessageAsync } = useSignMessage();
 
   const handleNodes = (newNode: any) => {
     setNodes((prevNodes: any) => [...prevNodes, newNode]);
-  }
+  };
 
   const handleLinks = (newLink: any) => {
     setLinks((prevLinks: any) => [...prevLinks, newLink]);
-  }
+  };
 
   const handleSelectedNodes = (d: any) => {
     setSelectedNodes((prevSelectedNodes: any) => {
@@ -31,32 +33,55 @@ export default function Home() {
         return [...prevSelectedNodes, d];
       }
     });
-  }
+  };
 
   const handleInput = async () => {
     if (!input.trim()) return;
 
+    const signature = await signMessage();
+    if (!signature) return;
+
+    const cid = createNewNode(input, selectedNodes);
+
+    await mintAndRegisterNFT();
+
+    setInput("");
+  };
+
+  const signMessage = async () => {
+    const message = "Sign this message to verify you’re the owner.";
+    try {
+      const signature = await signMessageAsync({ message });
+      console.log("signature created:", signature);
+      return signature;
+    } catch (err) {
+      console.error("signature failed or was rejected:", err);
+      toast.error("Signature is required to continue.");
+      return null;
+    }
+  };
+
+  const createNewNode = (input: string, selectedNodes: any[]) => {
     const newNode = {
       message: input,
       data: [],
-      children: [] as any[]
-    }
+      children: [] as any[],
+    };
     const cid = generateCid(newNode);
     selectedNodes.forEach((node: any) => {
       newNode.children.push(node.cid);
     });
-    setNodes((prevNodes: any) => [...prevNodes, {...newNode, cid, id: cid}])
+    setNodes((prevNodes: any) => [...prevNodes, { ...newNode, cid, id: cid }]);
     selectedNodes.forEach((node: any) => {
-      setLinks((prevLinks: any) => [
-        ...prevLinks, 
-        { source: cid, target: node.id }
-      ]);
-    })
+      setLinks((prevLinks: any) => [...prevLinks, { source: cid, target: node.id }]);
+    });
+    return cid;
+  };
 
-    const tid1 = toast.loading("Uploading image to IPFS...");
-    // TODO(jiyoung): upload image to IPFS
+  const mintAndRegisterNFT = async () => {
+    const tid = toast.loading("Uploading image to IPFS...");
     await new Promise((res) => setTimeout(res, 3000));
-    toast.success("IPFS upload completed. URI: https://ipfs.io/ipfs/Qm...", { id: tid1 });
+    toast.success("IPFS upload completed. URI: https://ipfs.io/ipfs/Qm...", { id: tid });
 
     // toast.loading('Creating NFT metadata...');
     // TODO(jiyoung): create nft metadata
@@ -68,7 +93,10 @@ export default function Home() {
     // TODO(jiyoung): created ip metadata
     // TODO(jiyoung): upload json metadata to IPFS
     await new Promise((res) => setTimeout(res, 3000));
-    toast.success('IP metadata created: { name: "KryptoPlanet #001", description: "화성 에레보스 평원 발견...", image: "ipfs://QmArkData..." }', { id: tid2 });
+    toast.success(
+      'IP metadata created: { name: "KryptoPlanet #001", description: "화성 에레보스 평원 발견...", image: "ipfs://QmArkData..." }',
+      { id: tid2 }
+    );
 
     // TODO(jiyoung): sign with wallet
     // ...
@@ -78,10 +106,11 @@ export default function Home() {
     // ref1: https://docs.story.foundation/sdk-reference/ipasset#mintandregisteripassetwithpilterms
     // ref2: https://docs.story.foundation/concepts/programmable-ip-license/pil-flavors#flavor-%231%3A-non-commercial-social-remixing
     await new Promise((res) => setTimeout(res, 3000));
-    toast.success('IPA "KryptoPlanet #001" successfully registered on Story Protocol! IPA ID: 0xArkIPAssetAddress…', { id: tid3 });
-
-    setInput("");
-  };
+    toast.success(
+      'IPA "KryptoPlanet #001" successfully registered on Story Protocol! IPA ID: 0xArkIPAssetAddress…',
+      { id: tid3 }
+    );
+  }
 
   return (
     <div className="flex flex-col h-full w-full gap-5">
@@ -89,7 +118,14 @@ export default function Home() {
       {/* Contents */}
       <div className="flex flex-row gap-4">
         <FOLViewer />
-        <DagVisualizer nodes={nodes} links={links} handleLinks={handleLinks} handleNodes={handleNodes} selectedNodes={selectedNodes} handleSelectedNodes={handleSelectedNodes} />
+        <DagVisualizer
+          nodes={nodes}
+          links={links}
+          handleLinks={handleLinks}
+          handleNodes={handleNodes}
+          selectedNodes={selectedNodes}
+          handleSelectedNodes={handleSelectedNodes}
+        />
         <FeedVeiwer />
       </div>
       {/* Input */}
@@ -104,8 +140,11 @@ export default function Home() {
               e.preventDefault();
               handleInput();
             }
-          }}></input>
-        <button className="" onClick={handleInput}>Generate</button>
+          }}
+        ></input>
+        <button className="" onClick={handleInput}>
+          Generate
+        </button>
       </div>
     </div>
   );
